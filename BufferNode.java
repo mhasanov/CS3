d654cac164b8be44ae977c025681aabb0cbd51d9
aribali3 <74435662+aribali3@users.noreply.github.com>
2021-08-12 17:00:16 -0400

    Add files via upload

--- BufferNode.java
+++ BufferNode.java
@@ -1,160 +1,181 @@
-import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.ByteBuffer;
 
 /**
- * A node in a linked list that references a buffer, it's position in the file,
- * and whether it has been changed or not.
- *  @author Joshua Rush (jdrush89)
- *  @author Benjamin Roble (broble)
- *  @version Nov 2, 2011
+ * This class creates a BufferNode, which stores information regarding the
+ * buffer, its location in the program, and any modification made to it.
+ *
+ * @author aribali3
+ * @author Mehdi Hasanov
+ * @version Aug 08, 2021
  */
-public class BufferNode
-{
-    //The buffer this node is associated with.
-    private Buffer myBuffer;
-    //A flag to keep track of whether or not the buffer has changed.
-    private boolean changed;
-    //The starting position of the buffer in the file (the byte #)
-    private long blockID;
-    //A constant to represent the starting position of an empty buffer.
-    private static final int EMPTY = -1;
+public class BufferNode {
 
-    /**
-     * Initialize the BufferNode, creating a new Buffer and setting the blockID
-     * and changed flag.
-     */
-    public BufferNode() {
-        myBuffer = new Buffer();
-        blockID = EMPTY;
-        changed = false;
-    }
-    // ----------------------------------------------------------
-    /**
-     * Return the buffer associated with this BufferNode.
-     * @return the buffer
-     */
-    public Buffer getBuffer()
-    {
-        return myBuffer;
-    }
-    // ----------------------------------------------------------
-    /**
-     * Set the buffer associated with this BufferNode.
-     * @param myBuffer the buffer to set
-     */
-    public void setMyBuffer(Buffer myBuffer)
-    {
-        this.myBuffer = myBuffer;
-    }
-    // ----------------------------------------------------------
-    /**
-     * Return whether or not the buffer has been changed since it read in data.
-     * @return the whether the buffer has been changed
-     */
-    public boolean isChanged()
-    {
-        return changed;
-    }
-    // ----------------------------------------------------------
-    /**
-     * Set whether the buffer in this node has been changed.
-     * @param changed whether the buffer has been changed
-     */
-    public void setChanged(boolean changed)
-    {
-        this.changed = changed;
-    }
-    // ----------------------------------------------------------
-    /**
-     * Return the byte number where the data block starts for the buffer
-     * associated with this node.
-     * @return the blockID
-     */
-    public long getBlockID()
-    {
-        return blockID;
-    }
-    // ----------------------------------------------------------
-    /**
-     * Set the byte position from the file where the data in this node's buffer starts.
-     * @param startReadingPosition the blockID to set
-     */
-    public void setBlockID(long startReadingPosition)
-    {
-        this.blockID = startReadingPosition;
-    }
+	// private static final int EMPTY = -1;
+	private static final int BUFFER_EMP = -1;
+	// private boolean changed;
+	private boolean buffModified;
+	// private Buffer myBuffer;
+	private Buffer currBuff;
+	// private long blockID;
+	private long initialBuffPos;
 
-    protected class Buffer{
-    	private byte[] data;
-        /**
-         * Create a new Buffer, initializing the data array.
-         */
-        public Buffer()
-        {
-            data = new byte[BufferPool.BUFFER_SIZE];
-        }
-        // ----------------------------------------------------------
-        /**
-         * Return the byte array storing data.
-         * @return the byte array
-         */
-        public byte[] getData()
-        {
-            return data;
-        }
+	/**
+	 * Create a new BufferNode object.
+	 */
+	public BufferNode() {
+		currBuff = new Buffer();
+		initialBuffPos = BUFFER_EMP;
+		buffModified = false;
+	}
 
-        // ----------------------------------------------------------
-        /**
-         * Set the data byte array.
-         * @param data the data to set
-         */
-        public void setData(byte[] data)
-        {
-            this.data = data;
-        }
-        /**
-         * Set the bytes in the data array to be the bytes in the newData array,
-         * starting at the indicated position.  pos + newData.length should not be
-         * greater than data.length.
-         * @param pos the position to start overwriting bytes at
-         * @param newData the byte array containing the bytes to be copied
-         */
-        public void setRecord(int pos, byte[] newData)
-        {
-            for(int i = 0; i < newData.length; i++)
-            {
-                data[i + pos] = newData[i];
-            }
-        }
-        /**
-         * Return a record made up of a 2 byte key and 2 byte value.
-         * @param pos the byte position where the record starts
-         * @return the record
-         */
-        public byte[] getRecord(int pos)
-        {
-            byte[] record = new byte[4];
-            System.arraycopy(data, pos, record, 0, 4);
-            return record;
-        }
+	/**
+	 * Getter for currBuff.
+	 *
+	 * @return the buffer of this BufferNode.
+	 */
+	public Buffer getCurrBuff() {
+		// public Buffer getBuffer()
+		return currBuff;
+	}
 
-        /**
-         * Returns a short made up of the bytes in the array at position pos and
-         * pos + 1.
-         * @param pos the position in the data array to get the short from
-         * @return a short from the data array
-         */
-        public short getShort(int pos)
-        {
-            ByteBuffer bb = ByteBuffer.allocate(2);
-            bb.order(ByteOrder.BIG_ENDIAN);
-            bb.put(data[pos]);
-            bb.put(data[pos+1]);
-            short shortVal = bb.getShort(0);
-            if (shortVal < 0)
-                System.out.println("Short was negative: " + shortVal);
-            return shortVal;
-        }
-    }
+	/**
+	 * Obtains the location of the starting data block for the buffer.
+	 *
+	 * @return the Byte Number of the buffer.
+	 */
+	public long getInitialBuffPos() {
+		// public long getBlockID()
+		return initialBuffPos;
+	}
 
+	/**
+	 * Obtains the boolean value of whether the Buffer has been altered.
+	 *
+	 * @return true, if the buffer has been modified. False, otherwise.
+	 */
+	public boolean getBuffModified() {
+		// public boolean isChanged()
+		return buffModified;
+	}
+
+	/**
+	 * This method sets the location from the file where the data of the node buffer
+	 * begins.
+	 *
+	 * @param initialBlockID the long value that will be set.
+	 */
+	public void setInitialBuffPos(long initialBlockID) {
+		// public void setBlockID(long startReadingPosition)
+		this.initialBuffPos = initialBlockID;
+	}
+
+	/**
+	 * Setter for the buffModified.
+	 *
+	 * @param buffModified whether the node has been altered.
+	 */
+	public void setBuffModified(boolean buffModified) {
+		// public void setChanged(boolean changed)
+		this.buffModified = buffModified;
+	}
+
+	/**
+	 * Sets the current Buffer of this Node.
+	 *
+	 * @param currBuff the current Buffer.
+	 */
+	public void setcurrBuff(Buffer currBuff) {
+		// public void setMyBuffer(Buffer myBuffer)
+		this.currBuff = currBuff;
+	}
+
+	/**
+	 * Helper class which will store the information in an array. It also provides
+	 * getters and setters for those attributes.
+	 *
+	 * @author aribali3
+	 * @author Mehdi Hasanov
+	 * @version Aug 08, 2021
+	 */
+	protected class Buffer {
+		// private byte[] data;
+		private byte[] myBytes;
+
+		/**
+		 * Constructor for the Buffer Object.
+		 */
+		public Buffer() {
+			myBytes = new byte[BufferPool.BUFFER_SIZE];
+		}
+
+		/**
+		 * Obtains the record composed of byte key and value.
+		 *
+		 * @param index the index where the data begins.
+		 * @return the newerBytes Array.
+		 */
+		public byte[] getDataRecords(int index) {
+			// public byte[] getRecord(int pos)
+			int value = 4;
+			byte[] tempByte = new byte[value];
+			int initialVal = 0;
+			System.arraycopy(myBytes, index, tempByte, initialVal, value);
+			return tempByte;
+		}
+
+		/**
+		 * Obtains the value comprised of bytes at the (index) and (index + 1) position.
+		 *
+		 * @param index The index in the array where the value will be obtained.
+		 * @return value from the array.
+		 */
+		public short getShort(int index) {
+			// public short getShort(int pos)
+			int allocateSize = 2;
+			ByteBuffer myByteBuff = ByteBuffer.allocate(allocateSize);
+			myByteBuff.order(ByteOrder.BIG_ENDIAN);
+			myByteBuff.put(myBytes[index]);
+			myByteBuff.put(myBytes[index + 1]);
+			short result = myByteBuff.getShort(0);
+			if (result < 0) {
+				System.out.println("The value=  " + result + "was less than 0");
+			}
+			return result;
+		}
+
+		/**
+		 * Getter for the myBytes array.
+		 *
+		 * @return elements in the array.
+		 */
+		public byte[] getmyBytes() {
+			// public byte[] getData()
+			return myBytes;
+		}
+
+		/**
+		 * Setter for the myBytes Array.
+		 *
+		 * @param myBytes the data the will be set.
+		 */
+		public void setMyBytes(byte[] myBytes) {
+			// public void setMyBytes(byte[] data)
+			this.myBytes = myBytes;
+		}
+
+		/**
+		 * This method will set the bytes in myBytes beginning at the position
+		 * specified.
+		 *
+		 * @param index this is the index where bytes will be overwritten.
+		 * @param items the bytes that will be written to the previous bytes.
+		 */
+		public void setNewerBytes(int index, byte[] items) {
+			// public void setRecord(int pos, byte[] newData)
+			for (int j = 0; j < items.length; j++) {
+				myBytes[j + index] = items[j];
+			}
+		}
+	}
 }
